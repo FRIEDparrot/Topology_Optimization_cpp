2880a6b2e21fb1903e64c55eee4eb8a34a266e41
FRIEDparrot <94303203+FRIEDparrot@users.noreply.github.com>
2022-12-22 14:15:05 +0800

    finished a piece of the Topo cpp

--- Topo_cpp/Topo_cpp/Fun_assertion.h
+++ Topo_cpp/Topo_cpp/Fun_assertion.h
@@ -1,55 +1,118 @@
+#pragma once
 #ifndef FUN_ASSERTION
 #define FUN_ASSERTION
 #include <iostream>
 #include <string.h>
 #include <assert.h>    // this package is used for raise error
-
 #define PI 3.1415
 
 using namespace std;
+void FEM(int xelm, int yelm);
 
 // The definition of the class should be put here. 
-class DyMat {  // global parameters definition
+
+template <typename ItemType>
+class DyArray {
 public:
-	DyMat(int xelm, int yelm) {
-		//  initialize an array of dynamic size
-		this->xelm = xelm;
-		this->yelm = yelm;
-		this->Mat = new int* [xelm];    
-		// attention the initialization of the Matrix with a userdefined boudary should be placed in initialization function
-		// define a pointer which is for a 2-D array -> 
-		// in this case we can initialize an 2-D array with predefined-size
-		
-		for (int j = 0; j < yelm; j++) {
-			*(this -> Mat + j) = new int[yelm];
-		}
-		for (int i = 0; i < xelm; i++) {
-			for (int j = 0; j < yelm; j++) {
-				*(*(this -> Mat + i) + j) = 0;   // initialize the corresponding element of the pointer array
-			}
-		}
-	};
-	void set(int i,int j, value) {
-		if (i >= xelm || j >= yelm || i<0 || j < 0) {
-			throw std::runtime_error("the index out of boundary");
-		}
-		else {
-			
+	int length;
+	DyArray(int length, ItemType InitValue) {
+		this->length = length;
+		this->Array = new ItemType[length];
+		for (int i = 0; i < length; i++) {
+			*(this->Array + i) = InitValue;
 		}
 	}
-	void Display_Content(){
-		for (int i = 1; i < xelm; i++) {
-			for (int j = 1; j < yelm; j++) {
-				cout << *(*(this->Mat + i) + j) << " ";
-			}
-			cout << "\n";
+	void set(int index, ItemType value) {
+		try {
+			*(this->Array + index) = value;
 		}
-	};
+		catch (runtime_error &e) {
+			throw std::runtime_error("an error occurred!");
+			cout << e.what();
+		}
+	}
+	ItemType get(int index) {
+		return *(this -> Array + index);
+	}
+	void Display_Content() {
+		for (int i = 0; i < length; i++) {
+			cout << *(this -> Array + i) << " ";
+		}
+		cout << endl;
+	}
 private:
-	int xelm;
-	int yelm;
-	int** Mat;
+	ItemType* Array;
 };
+
+DyArray<int> Apply_fixed_constraint(DyArray<int> freedof, int nelx, int nely);
+
+// -----------------dynamic array----------------------
+template <typename ItemType>
+class DyMat {  // global parameters definition
+	public:
+		int xelm;
+		int yelm;
+		DyMat(int xelm, int yelm, ItemType InitValue){
+			//  initialize an array of dynamic size
+			this->xelm = xelm;
+			this->yelm = yelm;
+			this->Mat = new ItemType* [xelm];
+			// attention the initialization of the Matrix with a userdefined boudary should be placed in initialization function
+			// define a pointer which is for a 2-D array -> 
+			// in this case we can initialize an 2-D array with predefined-size
+			for (int i = 0; i < xelm; i++) {
+				*(this -> Mat + i) = new ItemType[yelm];
+			}
+			for (int i = 0; i < xelm; i++) {
+				for (int j = 0; j < yelm; j++) {
+					*(*(this -> Mat + i) + j) = InitValue;   // initialize the corresponding element of the pointer array
+				}
+			}
+		};
+		void set(int i, int j, ItemType value) {
+			try {
+				*(*(this->Mat + i) + j) = value;
+			}
+			catch (runtime_error& e) {
+				throw std::runtime_error("the index out of boundary");
+				cout << e.what();
+			}
+		};
+		// This funciton is used to set a peice of chunk;
+		void setChunk(int lines[], int cols[], DyMat<ItemType> dmatrix ) {
+			// there we initilize the lines[] as a pointer --> so we can't get it's size
+
+			int lineNum = dmatrix.xelm;
+			int colNum = dmatrix.yelm;
+			for (int i = 0; i < dmatrix.xelm; i++) {
+				cout << i;
+			};
+			cout << "\n";
+			for (int i = 0; i < lineNum ; i++) {
+				for (int j = 0; j < colNum ; j++){
+					int x_i = lines[i];
+					int y_i = cols[j];
+					cout << x_i << " " << y_i << endl;
+				}
+			}
+			
+		};
+
+		ItemType get(int i, int j) {
+			return *(*(this->Mat + i) + j);
+		}
+		void Display_Content(){
+			for (int i = 0; i < xelm; i++) {
+				for (int j = 0; j < yelm; j++) {
+					cout << *(*(this->Mat + i) + j) << " ";
+				}
+				cout << "\n";
+			}
+		};
+	private:
+		ItemType** Mat;
+};
+
 
 // class DyMat(int,int);
 // the function in the other cpp source file can be asserted
--- Topo_cpp/Topo_cpp/Topo_cpp.cpp
+++ Topo_cpp/Topo_cpp/Topo_cpp.cpp
@@ -2,13 +2,48 @@
 #include <iostream>
 #include "Fun_assertion.h"    // attention that should
 
-
 int main()
 {
-    DyMat D(20,20);
-    D.Display_Content();
+    FEM(4, 3);
+
     // we had "using namespace std" in the head file
     return 0;
 }
 
-// also the assert of the functions (if we don't want to put it before the head)
+void FEM(int xelm, int yelm) {
+    // initialize the dynamic_matrix
+    DyMat<double> K(2 * (xelm + 1) * (yelm + 1), 2 * (xelm + 1) * (yelm + 1), 0.);  // this is the method to initialize 
+    DyMat<double> F(2 * (xelm + 1), 1, 0.);
+    DyMat<double> U(2 * (xelm + 1), 1, 0.);
+    DyMat<double> TestMat(2, 2, 5.3);
+    // =========initialize the boudary condition =============
+    F.set(2, 1, -1.);
+    // set the value of the F matrix;
+    DyArray<int> alldofs(2 * (xelm + 1) * (yelm + 1), 1);  // firstly we set all dimension as free
+    Apply_fixed_constraint(alldofs, xelm, yelm);
+
+    int xline[] {0,1};
+    int yline[] {2,3};
+    K.setChunk(xline, yline,TestMat);
+
+
+    // alldofs.Display_Content();  ---> then we get the 
+
+
+    // --------------- matlab code ---------------
+    // fixeddofs = [1: 2 : 2 * (nely + 1)];
+    // alldofs = [1:2 * (nely + 1) * (nelx + 1)];
+    // freedofs = setdiff(alldofs, fixeddofs);
+    // fixeddofs = union([1:2 : 2 * (nely + 1)], [2 * (nelx + 1) * (nely + 1)]);
+};
+
+DyArray<int> Apply_fixed_constraint(DyArray<int> freedof, int xelm, int yelm) {
+    // apply a fixed constraint on the left of the beam;  --> that part is the definition of the matrix
+    for (int i = 0; i < yelm + 1; i ++) {
+        freedof.set(2*i*( xelm + 1)+1, 0);
+    }
+    freedof.set(2*(xelm + 1) * (yelm + 1) - 1, 0); 
+    // attention the last element should be length-1
+    return freedof;
+}
+
